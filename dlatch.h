fcc6b55b4d66af64e62701b6627f166f8fc75527
rookie1024 <rookie1286@gmail.com>
2016-03-12 09:58:07 -0800

    Ran file through Visual Studio's formatter

--- dlatch.h
+++ dlatch.h
@@ -2,45 +2,45 @@
 #define RKLOGIC_DLATCH
 
 typedef struct {
-	bool inLast,
-		input,
-		out;
+  bool inLast,
+    input,
+    out;
 } DLatch;
 
 void resetDLatch(DLatch *latch, bool input) {
-	latch->inLast = input;
-	latch->out = 0;
+  latch->inLast = input;
+  latch->out = 0;
 }
 
 void updateDLatch(DLatch *latch, bool input) {
-	latch->inLast = latch->input;
-	latch->input = input;
+  latch->inLast = latch->input;
+  latch->input = input;
 }
 
 bool transient(DLatch *latch, bool input) {
-	updateDLatch(latch, input);
+  updateDLatch(latch, input);
 
-	return latch->out = latch->input ^ latch->inLast;
+  return latch->out = latch->input ^ latch->inLast;
 }
 
 bool risingEdge(DLatch *latch, bool input) {
-	updateDLatch(latch, input);
+  updateDLatch(latch, input);
 
-	return latch->out = latch->input && !latch->inLast;
+  return latch->out = latch->input && !latch->inLast;
 }
 
 bool fallingEdge(DLatch *latch, bool input) {
-	updateDLatch(latch, input);
+  updateDLatch(latch, input);
 
-	return latch->out = !latch->input && latch->inLast;
+  return latch->out = !latch->input && latch->inLast;
 }
 
 bool risingBistable(DLatch *latch, bool input) {
-	updateDLatch(latch, input);
+  updateDLatch(latch, input);
 
-	if (latch->input && !latch->inLast) latch->out = !latch->out;
+  if (latch->input && !latch->inLast) latch->out = !latch->out;
 
-	return latch->out;
+  return latch->out;
 }
 
 #endif
